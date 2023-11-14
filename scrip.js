d6e5aad04d9ec78eb67d0fac782522ae3e3f1343
Rafael Bastos <rafael_bastos30@hotmail.com>
2023-11-13 21:07:43 -0300

    Adicionando JavaScript

---
+++ scrip.js
@@ -0,0 +1,25 @@
+function alerta(){
+    alert("Olá")
+}
+
+function pergunta(){
+    prompt("Qual seu nome?")
+}
+
+function confirma(){
+    confirm("Você é humano?")
+}
+
+function mesclando(){
+    var nome = prompt("Fala meu nobre! Qual seu nome?")
+    alert("Seu nome é " +nome)
+}
+
+function conta(){
+var nome = prompt("Fala meu nobre! Qual seu nome?")
+    alert("Seu nome é " +nome)
+var n1 = Number(prompt(nome+", preciso que você digite um número:"));
+var n2 = Number(prompt(nome+", preciso que você digite outro número:"));
+    n3 = n1 + n2;
+    alert("A soma dos números é igual a: "+n3)
+}
